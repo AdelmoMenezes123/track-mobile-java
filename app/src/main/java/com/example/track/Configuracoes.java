b8006170f4502ada5a0bbb8f4a7397c74628b31a
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-09-16 00:03:51 -0300

    inicia criacao de tela configuracao

--- app/src/main/java/com/example/track/Configuracoes.java
+++ app/src/main/java/com/example/track/Configuracoes.java
@@ -10,22 +10,22 @@ import android.widget.TextView;
 
 public class Configuracoes extends AppCompatActivity {
 
-    TextView voltaHome;
+    //TextView voltaHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
 
-        IniciaComponent();
-        voltaHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent in = new Intent(Configuracoes.this, Home.class);
-                startActivity(in);
-            }
-        });
+        //    IniciaComponent();
+        //voltaHome.setOnClickListener(new View.OnClickListener() {
+        //   @Override
+        //  public void onClick(View view) {
+        //      Intent in = new Intent(Configuracoes.this, Home.class);
+        //      startActivity(in);
+        //  }
+        //});
     }
-    private void IniciaComponent(){
-        voltaHome = (TextView) findViewById(R.id.voltar);
-    }
+    //private void IniciaComponent(){
+    //  voltaHome = (TextView) findViewById(R.id.voltar);
+    //}
 }
