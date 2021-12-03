4c0cb8bea709048e529fcbb692b1708f01fc41e7
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-12-03 20:19:06 -0300

    finaliza projeto

--- app/src/main/java/com/example/track/Home.java
+++ app/src/main/java/com/example/track/Home.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
-    View entrarConfig, entrarHistorico, entrarCredito, entrarNavegacao;
+    View entrarConfig, entrarHistorico, entrarCredito, entrarNavegacao, entrarGnss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +49,20 @@ public class Home extends AppCompatActivity {
             }
         });
 
+        entrarGnss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Home.this, Gnss.class);
+                startActivity(in);
+            }
+        });
+
     }
     private void IniciaComponent(){
         entrarConfig = (View) findViewById(R.id.configuracoes);
         entrarCredito = (View) findViewById(R.id.creditos);
         entrarHistorico = (View) findViewById(R.id.historico);
         entrarNavegacao = (View) findViewById(R.id.navegacao);
+        entrarGnss = (View) findViewById(R.id.gnss);
     }
 }
